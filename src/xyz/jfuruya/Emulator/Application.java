f9df248667ec31cef5096ed55cfb643aafc8054c
Jun Furuya <jun.fuluya@gmail.com>
2020-01-17 19:02:52 +0900

    JFrame, ウィンドウの×ボタンの処理、メニューバー追加（イベントは未実装）

---
+++ src/xyz/jfuruya/Emulator/Application.java
@@ -0,0 +1,13 @@
+package xyz.jfuruya.Emulator;
+import views.MyFrame;
+
+public class Application {
+
+	private static MyFrame frame = null;
+
+	public static void main(String[] args) {
+		frame = MyFrame.getInstance();
+		frame.build();
+	}
+
+}
